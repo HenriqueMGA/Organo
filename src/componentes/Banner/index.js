c91b58313d8e2f6908244029598a387493488916
HenriqueMGA <riquemamaral@gmail.com>
2024-06-09 21:56:51 -0300

    Organo

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <div className="banner">
+            <img src="/imagens/banner.png" alt="Imagem Banner"/>
+        </div>
+    )
+}
+
+export default Banner
---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,17 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (
+        <div className='rodape'>
+            <div className='redes__sociais'>
+                <a href=''><img src='./imagens/fb.png' alt=''/></a>
+                <a href=''><img src='./imagens/tw.png' alt=''/></a>
+                <a href=''><img src='./imagens/ig.png' alt=''/></a>
+            </div>
+            <img className='logo' src='./imagens/logo.png' alt=''/>
+            <p>Desenvolvido por Alura/Henrique Melo</p>
+        </div>
+    )
+}
+
+export default Rodape
